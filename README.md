4deffcf67555f249a970456a0e512001644a9b0d
Anthony Saliba <anthony.s2501protonmail.com@proton.me>
2024-11-23 22:34:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Voidbox
+
+A modern toolbox web app that emphasizes on fulfilling multiple use cases easily and quickly, while keeping a solid design.
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <>
+      <div className="w-full min-h-dvh m-auto flex justify-center items-center p-4"></div>
+    </>
+  );
+};
